f3684e684950a2093293792b01dba35198460793
Abdullah007bajwa <bajwa15523@gmail.com>
2025-08-05 13:40:51 +0500

    Updates

---
+++ js/Footer-BmXXc9Rl.js
@@ -0,0 +1,6 @@
+import{j as e}from"./three-vendor-Cj4undDh.js";import{c as t}from"../assets/index-D753pGTp.js";import"./react-vendor-CwEH9sB7.js";import"./ui-vendor-HjUFXU1T.js";import"./utils-vendor-B20fFAH1.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=t("Heart",[["path",{d:"M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z",key:"c3ymky"}]]),m=()=>e.jsx("footer",{className:"border-t border-border/50 py-8",children:e.jsx("div",{className:"container mx-auto px-4",children:e.jsxs("div",{className:"text-center",children:[e.jsxs("p",{className:"text-muted-foreground flex items-center justify-center gap-2",children:["Made with ",e.jsx(r,{className:"w-4 h-4 text-primary fill-current"})," by"," ",e.jsx("span",{className:"gradient-text font-semibold",children:"Abdullah Bajwa"})]}),e.jsx("p",{className:"text-sm text-muted-foreground mt-2",children:"© 2024 All rights reserved."})]})})});export{m as default};
